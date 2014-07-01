6a9cdf127bb69d5d3c605af64307cb5577c5e01f
Ludwig Kraatz <ludwig.kraatz@suncircle.de>
2014-07-01 23:54:36 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import finddata
+from setuptools import setup, find_packages
+
+setup(
+    name="introspective_api",
+    author="Ludwig Kraatz",
+    author_email="code@suncircle.de",
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+)
