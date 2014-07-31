af6deafb137291f887a511da11719d4303131c54
Ludwig Kraatz <ludwig.kraatz@suncircle.de>
2014-07-31 16:47:28 +0200

    added requirements into setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup, find_packages
 
+
+def get_path(fname):
+    return os.path.join(os.path.dirname(__file__), fname)
+
+
+def read(fname):
+    return open(get_path(fname)).read()
+
+
 setup(
     name="introspective_api",
     author="Ludwig Kraatz",
@@ -7,4 +16,5 @@ setup(
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
+    install_requires=read('requirements.txt').split("\n"),
 )
