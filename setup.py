8e8d0f6cbef21e04c54d7c43343883a31aa3e22e
Ludwig Kraatz <ludwig.kraatz@suncircle.de>
2014-08-02 01:04:03 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ dependencies = []
 for requirement in parse_requirements('requirements.txt'):
     if requirement.url:
         url = str(requirement.url)
-        dependencies.append(url + '==' + url.split('-')[-1])
+        dependencies.append(url)
+        requirements.append(str(requirement.req) + '==' + url.split('-')[-1])
     else:
         requirements.append(str(requirement.req))
 
@@ -17,7 +18,7 @@ setup(
     name="introspective-api",
     author="Ludwig Kraatz",
     author_email="code@suncircle.de",
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
