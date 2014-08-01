75797621e2e4d8f938aa49d5c8e9232a09ceda00
Ludwig Kraatz <ludwig.kraatz@suncircle.de>
2014-08-02 01:48:16 +0200

    updated setup.py & dependencies

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     name="introspective-api",
     author="Ludwig Kraatz",
     author_email="code@suncircle.de",
-    version='0.1.8',
+    version='0.1.9',
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
