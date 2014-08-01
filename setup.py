131697e1aad00964517fcab7c47b656b25a1e8ab
Ludwig Kraatz <ludwig.kraatz@suncircle.de>
2014-08-01 23:23:28 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ dependencies = []
 
 
 for requirement in parse_requirements('requirements.txt'):
-    requirements.append(str(requirement.req))
     if requirement.url:
         dependencies.append(str(requirement.url))
+    else:
+        requirements.append(str(requirement.req))
 
 
 setup(
