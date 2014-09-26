1e3a1a96dd7f67de307ea5495b0c7f33f29cecca
Ludwig Kraatz <ludwig.kraatz@suncircle.de>
2014-09-26 22:35:08 +0200

    updating version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     name="introspective-api",
     author="Ludwig Kraatz",
     author_email="code@suncircle.de",
-    version='0.1.10',
+    version='0.1.11',
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
